7b995f92ef94ec9dfd28c4c393aadfc121f9d349
Jin Park <jinpark1504@gmail.com>
2024-07-11 14:37:35 +0900

    testWhenFetchingSuccceedsPublishesSectionsBuiltFromReceivedMenuAndGivenGroupingCategoryClosure;

--- Albertos/AlbertosTests/MenuListViewModelTests.swift
+++ Albertos/AlbertosTests/MenuListViewModelTests.swift
@@ -6,9 +6,11 @@
 //
 
 import XCTest
+import Combine
 @testable import Albertos
 
 final class MenuListViewModelTests: XCTestCase {
+    var cancellables = Set<AnyCancellable>()
     func testCallsGivenGroupingFunction() {
         var called = false
         let inputSections = [MenuSection.fixture()]
@@ -30,5 +32,33 @@ final class MenuListViewModelTests: XCTestCase {
     func testWhenFetchingStartsPublishesEmptyMenu() {
         let viewModel = MenuList.ViewModel()
         XCTAssertTrue(viewModel.sections.isEmpty)
+    }
+    
+    func testWhenFetchingSuccceedsPublishesSectionsBuiltFromReceivedMenuAndGivenGroupingCategoryClosure() {
+        var receivedMenu: [MenuItem]?
+        let expectedSections: [MenuSection] = [.fixture()]
+        
+        let spyClosure: ([MenuItem]) -> [MenuSection] = { items in
+            receivedMenu = items
+            return expectedSections
+        }
+        
+        let viewModel = MenuList.ViewModel(
+            menuFetching: MenuFetchingSample(),
+            menuGrouping: spyClosure
+            )
+        
+        let expectation = XCTestExpectation(description: "받은 메뉴와 주어진 그룹화 클로저를 사용하여 생성된 섹션들을 발행함")
+        
+        viewModel.$sections
+            .dropFirst()
+            .sink { value in
+                XCTAssertEqual(receivedMenu, menu)
+                XCTAssertEqual(value, expectedSections)
+                expectation.fulfill()
+            }
+            .store(in: &cancellables)
+        
+        wait(for: [expectation], timeout: 1)
     }
 }
